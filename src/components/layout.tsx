5bebddfaed170a3b02516991ff7429c462505e90
AnnuityDew <annuitydew@gmail.com>
2021-02-14 01:46:42 -0500

    I think I added Google Analytics?

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -14,6 +14,12 @@ const Layout = ({ children, bgColor = `#000000` }: LayoutProps) => (
       <Header bgColor="#222222" />
       <Main bgColor={bgColor}>{children}</Main>
       <Footer bgColor="#222222" />
+      {/* The core Firebase JS SDK is always required and must be listed first. 
+        Add SDKs for Firebase products that you want to use.
+        https://firebase.google.com/docs/web/setup#available-libraries*/}
+      <script src="/__/firebase/8.2.7/firebase-app.js"></script>
+      <script src="/__/firebase/8.2.7/firebase-analytics.js"></script>
+      <script src="/__/firebase/init.js"></script>
     </LayoutWrapper>
   </React.Fragment>
 )
