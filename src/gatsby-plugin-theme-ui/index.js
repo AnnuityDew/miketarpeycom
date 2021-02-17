3d558819df7b84fd4409cd67fb26c8fb496ab8cb
AnnuityDew <annuitydew@gmail.com>
2021-02-16 21:56:46 -0500

    Less reliance on theme file

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,10 +1,4 @@
 export default {
-  colors: {
-    text: "#FFFFFF",
-    background: "#000000",
-    primary: "#639",
-    secondary: "#ff6347",
-  },
   breakpoints: [`640px`, `768px`, `1024px`, `1280px`],
   fontWeights: {
     normal: 400,
